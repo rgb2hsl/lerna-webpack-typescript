7dec5a681d2dd7bb7eb8c51d8c4a9acb4a05eb45
ru-web-designer <ru.web.designer@gmail.com>
2018-09-14 00:17:39 +0300

    improved functionality

--- packages/woopler/index.ts
+++ packages/woopler/index.ts
@@ -2,18 +2,22 @@ import * as $ from "jquery";
 
 export class Woopler {
     asyncConsoleLog(message: string = "") {
-        $.ajax("https://api.thecatapi.com/v1/images/search?", {
-            dataType: 'jsonp',
+        $.ajax("https://api.thecatapi.com/v1/images/search?mime_type=jpg,png", {
             success: (data:{
                 url: string;
             }[]) => {
-                const catImage = data.pop();
+                const img = data.pop();
 
-                if (catImage) {
-                    $(document).append(`<img src="${catImage.url}">`);
+                if (img) {
+                    $(`<img src="${img.url}">`)
+                        .one('load', () => {
+                            console.log(message);
+                        })
+                        .appendTo('body')
+
+                } else {
+                    console.log(`Sorry, no cats today :( btw message was '${message}'`);
                 }
-
-                console.log(message);
             }
         });
     }
